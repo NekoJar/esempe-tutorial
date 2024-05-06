3564000ffdfb5e6499ecc7ee2575687f09615a3e
kira <estimate.line@gmail.com>
2024-05-06 22:55:40 +0700

    01.5: resolving error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { Hero } from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
       <Hero />
     </main>
   );
